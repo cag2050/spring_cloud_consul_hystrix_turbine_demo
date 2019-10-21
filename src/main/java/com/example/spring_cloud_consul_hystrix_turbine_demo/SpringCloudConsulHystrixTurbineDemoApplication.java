705df4bf1b6ed0d47133fadcbee2d53ab87cc5df
chen_anguo <cag2050@163.com>
2019-10-21 15:50:58 +0800

    project init

---
+++ src/main/java/com/example/spring_cloud_consul_hystrix_turbine_demo/SpringCloudConsulHystrixTurbineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_consul_hystrix_turbine_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulHystrixTurbineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulHystrixTurbineDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_cloud_consul_hystrix_turbine_demo/SpringCloudConsulHystrixTurbineDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_consul_hystrix_turbine_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsulHystrixTurbineDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
